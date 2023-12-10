6cf7ebe05bb0e4daec3ebc9d9cfbc92223a7d3d6
gedira27 <20030013@itcelaya.edu.mx>
2023-12-09 23:55:31 -0600

    App funcionando

---
+++ src/main/java/org/example/impresiones/ButtonThread.java
@@ -0,0 +1,68 @@
+package org.example.impresiones;
+
+import javafx.application.Platform;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+public class ButtonThread extends Thread{
+    public static Queue<ElementoTabla> listaElementos = new ConcurrentLinkedQueue<>();
+    private boolean btnActivo = true;
+    public static boolean onAnimation = false;
+    private Label lblOn;
+    public HBox hBox;
+    private Integer contador = 1;
+    public ButtonThread() {
+        Button btnOn = new Button("Pausar/Resumir");
+        btnOn.setOnAction(actionEvent -> {
+            // si activo está On
+            if (btnActivo) {
+                lblOn.setText("OFF");
+                lblOn.setTextFill(Color.RED);
+                btnActivo = false;
+            } else {// si no, está apagado
+                lblOn.setText("ON");
+                lblOn.setTextFill(Color.GREEN);
+                btnActivo = true;
+            }
+        });
+
+        Button btnAdd = new Button("Agregar una impresion");
+        btnAdd.setOnAction(actionEvent -> {
+            // Agregar datos de ejemplo
+            ElementoTabla t1 = new ElementoTabla(contador, 0.0, ((int) (Math.random()*100)));
+            TableViewWithProgressIndicator.tabla.getItems().add(t1);
+            listaElementos.add(t1);
+            contador++;
+        });
+
+        lblOn = new Label("On");
+        lblOn.setTextFill(Color.GREEN);
+
+        hBox = new HBox(btnOn, lblOn, btnAdd);
+        hBox.setSpacing(30);
+    }
+    @Override
+    public void run()
+    {
+        super.run();
+        while(true) {
+            System.out.println("Entro");
+            if(btnActivo && !listaElementos.isEmpty() && !onAnimation) {
+                Platform.runLater(() -> {
+                    assert listaElementos.peek() != null;
+                    listaElementos.peek().onAnimation();
+                });
+            }
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
---
+++ src/main/java/org/example/impresiones/ElementoTabla.java
@@ -0,0 +1,119 @@
+package org.example.impresiones;
+
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.util.Duration;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class ElementoTabla {
+    private final int numHojas;
+    private final Integer no;
+    private final String nombre;
+    private final DoubleProperty progress;
+    private String hojas;
+    private final Integer time = 10;
+    public ElementoTabla(Integer no, double progress, int hojas) {
+        this.no = no;
+        this.nombre = generarNombre();
+        this.progress = new SimpleDoubleProperty(progress);
+        this.hojas = hojas+"";
+        this.numHojas = hojas;
+    }
+
+    public String getName() {
+        return nombre;
+    }
+
+    public Double getProgress() {
+        return progress.get();
+    }
+
+    public DoubleProperty progressProperty() {
+        return progress;
+    }
+
+    public Integer getNo() {
+        return no;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getHojas() {
+        return hojas;
+    }
+
+    public void setProgress(double progress) {
+        this.progress.set(progress);
+    }
+
+    public void onAnimation() {
+        animate();
+    }
+
+    private void animate() {
+        // Se cambia el proceso a que ya existe una animación en proceso
+        ButtonThread.onAnimation = true;
+        // Se crea la animacion con 10s
+        Timeline timeline = new Timeline(
+                new KeyFrame(Duration.ZERO, new KeyValue(progress, 0.0)),
+                new KeyFrame(Duration.seconds(time), new KeyValue(progress, 1.0))
+        );
+        // Se configura el proceso final cuando termina la animación
+        timeline.setOnFinished(event -> {
+            // se cambia la variable para saber que no hay ninguna animación actualmente
+            ButtonThread.onAnimation = false;
+            // método poll para traer el elemento cabeza y eliminarlo
+            // Se elimina el elemento de la tabla
+            TableViewWithProgressIndicator.tabla.getItems().remove(ButtonThread.listaElementos.poll());
+        });
+
+        Timeline labelUpdateTimeline = new Timeline(
+                new KeyFrame(Duration.ZERO, e -> updateHojas(0)),
+                new KeyFrame(Duration.seconds(1), e -> updateHojas(.1)),
+                new KeyFrame(Duration.seconds(2), e -> updateHojas(.2)),
+                new KeyFrame(Duration.seconds(3), e -> updateHojas(.30)),
+                new KeyFrame(Duration.seconds(4), e -> updateHojas(.40)),
+                new KeyFrame(Duration.seconds(5), e -> updateHojas(.50)),
+                new KeyFrame(Duration.seconds(6), e -> updateHojas(.60)),
+                new KeyFrame(Duration.seconds(7), e -> updateHojas(.70)),
+                new KeyFrame(Duration.seconds(8), e -> updateHojas(.80)),
+                new KeyFrame(Duration.seconds(9), e -> updateHojas(.90)),
+                new KeyFrame(Duration.seconds(1), e -> updateHojas(1.0))
+        );
+
+        labelUpdateTimeline.setCycleCount(1);
+
+        // se configura para que la animación nada más se realice 1 vez
+        timeline.setCycleCount(1);
+        // se inicia la animación
+        timeline.play();
+        labelUpdateTimeline.play();
+    }
+
+    private void updateHojas(double value) {
+        System.out.println("HACE ALGO");
+        this.hojas = ((int)(numHojas*value))+"/"+numHojas;
+        System.out.println(hojas);
+    }
+
+    private String generarNombre() {
+        String[] extensiones = {"png", "jpe", "xlsx", "pptx","pdf", "jpeg", "jpg", "tiff", "tif", "docx",};
+        // generar numero random para seleccionar la extension
+        int idExtension = (int) (Math.random() * extensiones.length);
+        // generar fecha y hora
+        // Obtener la fecha y hora actual
+        LocalDateTime currentDateTime = LocalDateTime.now();
+
+        // Formatear la fecha y hora
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd-HH:mm:ss");
+        String formattedDateTime = currentDateTime.format(formatter);
+        return formattedDateTime+extensiones[idExtension];
+    }
+}
